91924c429c42223a56a42bdc2502d87b46c69edf
NailMukhametshin <nail.zubr@gmail.com>
2018-12-12 21:53:19 +0300

    Переименовал переменную i в result

--- src/test/java/ru/itpark/DepositTest.java
+++ src/test/java/ru/itpark/DepositTest.java
@@ -1,5 +1,6 @@
 package ru.itpark;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,18 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class DepositTest {
 
     @Test
+    @DisplayName("Тесты на коректность расчетов")
     void calculateDeposit() {
-        // Some changes
+
         {
             Deposit deposit = new Deposit();
-            double i = deposit.calculateDeposit(100_000, 92, 12);
-            assertEquals(103_025, i);
+            double result = deposit.calculateDeposit(100_000, 92, 12);
+            assertEquals(103_025, result);
         }
 
         {
             Deposit deposit = new Deposit();
-            double i = deposit.calculateDeposit(10_000_000, 365, 7.5);
-            assertEquals(10_750_000, i);
+            double result = deposit.calculateDeposit(10_000_000, 365, 7.5);
+            assertEquals(10_750_000, result);
         }
     }
 }
